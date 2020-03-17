ca83467f467097e1ce7e7d79d8699dbc05f0d017
WRayz <ruizhouwu@gmail.com>
2020-03-18 00:39:50 +0800

    Read Excel to Objects

--- ExcelAdapter.cs
+++ ExcelAdapter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -26,16 +27,33 @@ namespace AfterServiceHelper
             using(var p = new ExcelPackage(file))
             {
                 var workSheet = p.Workbook.Worksheets["出貨日期"];
-                for (var columnIndex = 1; columnIndex <= workSheet.Dimension.Columns; columnIndex++)
+                for (var rowIndex = 2; rowIndex <= workSheet.Dimension.Rows; rowIndex++)
                 {
-                    System.Console.WriteLine($"{workSheet.Cells[1, columnIndex].Value}");
-                    // for (var rowIndex = 2; rowIndex <= workSheet.Dimension.Rows; rowIndex++)
-                    for (var rowIndex = 2; rowIndex <= 4; rowIndex++)
+                    _details.Add(new ShippedDetail
                     {
-                        System.Console.WriteLine($"{workSheet.Cells[rowIndex, columnIndex].Value}");
-                        _details.Add(new ShippedDetail
-                        { });
-                    }
+                        ProductType = workSheet.Cells[rowIndex, 1].Text,
+                            RobotSn = workSheet.Cells[rowIndex, 2].Text,
+                            ControlBox = workSheet.Cells[rowIndex, 3].Text,
+                            StickSn = workSheet.Cells[rowIndex, 4].Text,
+                            CustomerCode = workSheet.Cells[rowIndex, 5].Text,
+                            ShippedCustmer = workSheet.Cells[rowIndex, 6].Text,
+                            ShippedWeek = workSheet.Cells[rowIndex, 7].Text,
+                            ShippedDate = workSheet.Cells[rowIndex, 8].GetValue<DateTime?>(),
+                            Destination = workSheet.Cells[rowIndex, 9].Text,
+                            HmiVersion = workSheet.Cells[rowIndex, 10].Text,
+                            PowerBoard = workSheet.Cells[rowIndex, 11].Text,
+                            DriverFW = workSheet.Cells[rowIndex, 12].Text,
+                            IoVersion = workSheet.Cells[rowIndex, 13].Text,
+                            RtxSn = workSheet.Cells[rowIndex, 14].Text,
+                            ShippedMonth = workSheet.Cells[rowIndex, 15].GetValue<int?>(),
+                            ShippedYear = workSheet.Cells[rowIndex, 16].GetValue<int?>(),
+                            Remark = workSheet.Cells[rowIndex, 17].Text,
+                            Hw = workSheet.Cells[rowIndex, 18].Text,
+                            HwService = workSheet.Cells[rowIndex, 19].Text,
+                            Country = workSheet.Cells[rowIndex, 20].Text,
+                            Region = workSheet.Cells[rowIndex, 21].Text,
+                            CustomerType = workSheet.Cells[rowIndex, 22].Text
+                    });
                 }
             }
 
